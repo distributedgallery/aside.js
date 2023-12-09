77427f2587c7e77a51d3a1254df9949cde4f6a84
Olivier Sarrouy <me@osarrouy.bzh>
2023-12-09 18:13:16 +0100

    Update CORS policy

--- api/arxiv/index.js
+++ api/arxiv/index.js
@@ -1,6 +1,6 @@
 export default function handler(request, response) {
   console.log(request)
-  response.setHeader('Cache-Control', 'no-cache');
+  response.setHeader('Cache-Control', 'no-cache').setHeader('Access-Control-Allow-Credentials', true).setHeader('Access-Control-Allow-Origin', '*')
   response.status(200).json({
     nbOfPublications: Math.floor(Math.random() * 10) + 1
   });
