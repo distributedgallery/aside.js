f9f9743792f11f0e4a4d1ef26f33417004792fcb
Olivier Sarrouy <me@osarrouy.bzh>
2024-01-20 23:28:14 +0100

    Update sentiment to twitter request

--- api/aisentiment/update.js
+++ api/aisentiment/update.js
@@ -1,4 +1,33 @@
 import { kv } from '@vercel/kv';
+import needle from 'needle';
+
+const ENDPOINT = 'https://api.twitter.com/2/tweets/counts/recent';
+const PROS = ['#aiforgood', '#ai4good'];
+const CONS = ['#aiforbad', '#ai4bad'];
+
+function query(hashtags) {
+  let q = '';
+
+  for (let i = 0; i < hashtags.length; i += 1) {
+    if (i > 0) q += ' OR ';
+    q += hashtags[i];
+  }
+
+  return q;
+}
+
+async function count(hashtags) {
+  const response = await needle('get', ENDPOINT, { query: query(hashtags), granularity: 'day' }, {
+    headers: {
+      'User-Agent': 'v2RecentTweetCountsJS',
+      authorization: `Bearer ${process.env.TWITTER_BEARER_TOKEN}`,
+    },
+  });
+
+  if (response.body) { return response.body.meta.total_tweet_count; }
+
+  throw new Error('Unsuccessful Twitter request');
+}
 
 function isAuthorized(request) {
   return request.headers.authorization === `Bearer ${process.env.CRON_SECRET}`;
@@ -10,8 +39,14 @@ export default async function handler(request, response) {
       return response.status(401).json({ error: 'Unauthorized request' });
     }
 
+    const pros = await count(PROS);
+    const cons = await count(CONS);
+
+    if (pros === 0 && cons === 0) { throw new Error('No tweets found'); }
+
     const timestamp = Date.now();
-    const sentiment = Math.random();
+    const sentiment = (pros / (pros + cons)).toFixed(2);
+
     await kv.zadd(
       'sentiments',
       { score: timestamp, member: sentiment },
